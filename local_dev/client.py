f66694ee4518825a6ac9157f2128d5f75b0db04b
Guilherme Oliveira <e12243299@student.tuwien.ac.at>
2024-06-29 21:05:16 +0200

    measure time

--- local_dev/client.py
+++ local_dev/client.py
@@ -3,13 +3,16 @@ import base64
 import os
 import uuid
 import sys
+import time
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 def main(input_folder, endpoint):
+    times = []
     for image_filename in os.listdir(input_folder):
+        start = time.time()
         image_path = os.path.join(input_folder, image_filename)
         encoded_image = encode_image(image_path)
         image_id = str(uuid.uuid4())
@@ -19,6 +22,10 @@ def main(input_folder, endpoint):
         }
         response = requests.post(endpoint, json=payload)
         print(response.json())
+        end = time.time()
+        times.append(end - start)
+    average_time = sum(times) / len(times)
+    print(f"Average time: {average_time}")
 
 if __name__ == "__main__":
     input_folder = sys.argv[1]
